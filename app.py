c7b9b72cf729feacf466eb93b921ce9509aa8853
pratikwandhe <wandhepratik4@gmail.com>
2025-01-19 05:54:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from email.mime.text import MIMEText
 
 # Function to send email
 def send_email(recipient_email, student_name, days_remaining):
-    sender_email = "your_email@gmail.com"  # Replace with your Gmail address
-    sender_password = "your_app_password"  # Replace with your Gmail App Password
+    sender_email = "wandhepratik4@gmail.com"  # Replace with your Gmail address
+    sender_password = "Chaitu@123"  # Replace with your Gmail App Password
 
     subject = "Visa Renewal Notification"
     body = f"""Dear {student_name},
